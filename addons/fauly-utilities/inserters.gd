8c15cf2caf2362c31328674e52e40188529c80e2
fauly <faulyfaux@gmail.com>
2025-04-07 16:07:27 +0700

    missing an @ wrong function for datettime and shortcuts

--- addons/fauly-utilities/inserters.gd
+++ addons/fauly-utilities/inserters.gd
@@ -1,24 +1,21 @@
-tool
+@tool
 extends EditorPlugin
 
 var datetime_shortcut := Shortcut.new()
 
 func _enter_tree():
-	datetime_shortcut.shortcut = KeyEventShortcut.new()
-	datetime_shortcut.shortcut.shortcut = Shortcut.from_string("Ctrl+Shift+D")
-	
-	add_tool_menu_item("Insert DateTime", self, "insert_datetime", datetime_shortcut)
-	
+	add_tool_menu_item("Insert DateTime", Callable(self, "insert_datetime"))
+
 func _exit_tree():
 	remove_tool_menu_item("Insert DateTime")
-	
+
 func insert_datetime():
 	var editor_interface = get_editor_interface()
 	var script_editor = editor_interface.get_script_editor()
 	if script_editor:
 		var text_editor = script_editor.get_current_editor()
 		if text_editor:
-			var dt = OS.get_datetime()
+			var dt = Time.get_datetime_dict_from_system()
 			var formatted = "%04d-%02d-%02d %02d:%02d:%02d" % [
 				dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
 			]
