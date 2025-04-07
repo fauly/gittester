5d70cde96203fed06f0586eee10d2469da6ad9f2
fauly <faulyfaux@gmail.com>
2025-04-07 19:35:42 +0700

    Made the datetime inserter utility a command pallette command

--- addons/fauly-utilities/inserters.gd
+++ addons/fauly-utilities/inserters.gd
@@ -1,26 +1,26 @@
 @tool
 extends EditorPlugin
 
-var datetime_shortcut := Shortcut.new()
-
 func _enter_tree():
-	add_tool_menu_item("Insert DateTime", Callable(self, "insert_datetime"))
+    var palette = get_editor_interface().get_command_palette()
+    print("Reloaded utilities")
+    palette.add_command("Insert DateTime", "fauly_insert_datetime", Callable(self, "insert_datetime"),"Ctrl+D")
 
 func _exit_tree():
-	remove_tool_menu_item("Insert DateTime")
+    var palette = get_editor_interface().get_command_palette()
+    palette.remove_command("fauly_insert_datetime")
 
-func insert_datetime():
-	var editor_interface = get_editor_interface()
-	var script_editor = editor_interface.get_script_editor()
-	if script_editor:
-		var text_editor = script_editor.get_current_editor()
-		if text_editor:
-			var dt = Time.get_datetime_dict_from_system()
-			var formatted = "%04d-%02d-%02d %02d:%02d:%02d" % [
-				dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
-			]
-			text_editor.insert_text_at_cursor(formatted)
-		else:
-			print("No active text editor found.")
-	else:
-		print("Script editor not available.")
+func insert_datetime() -> bool:
+    var dt = Time.get_datetime_dict_from_system()
+    var formatted = "%04d-%02d-%02d %02d:%02d:%02d" % [
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+    ]
+
+    var editor = get_editor_interface().get_script_editor().get_current_editor().get_base_editor()
+
+    if editor:
+        editor.insert_text_at_caret(formatted)
+        return true
+    else:
+        print("No editor found to insert datetime.")
+        return false
