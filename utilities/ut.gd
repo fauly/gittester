081b1aeb13cdee100152f5f0de1202e5bf3c4f5a
fauly <faulyfaux@gmail.com>
2025-04-13 19:31:02 +0700

    Created utility for appending dir files to an array

--- utilities/ut.gd
+++ utilities/ut.gd
@@ -9,3 +9,21 @@ func camelCase(nameToChange: String) -> String:
 	for i in range(1, parts.size()):
 		result += parts[i].capitalize()
 	return result
+
+func appendDir(directory_path: String, target_array: Array) -> void:
+	var dir = DirAccess.open(ProjectSettings.globalize_path("res://"))
+	if dir == null:
+		push_error("Failed to initialize DirAccess for res://")
+
+	dir.list_dir_begin()
+	
+	var file_name = dir.get_next()
+	while file_name != "":
+		# Form the full path for the file.
+		var full_path = directory_path + "/" + file_name
+		# Check that the current item is not a directory.
+		if not dir.current_is_dir():
+			target_array.append(full_path)
+		file_name = dir.get_next()
+	
+	dir.list_dir_end()
