230573733ef831da87d4117f26bdcdbe999ad921
fauly <faulyfaux@gmail.com>
2025-04-13 23:05:31 +0700

    Added regex component to array directory appending

--- utilities/ut.gd
+++ utilities/ut.gd
@@ -10,20 +10,20 @@ func camelCase(nameToChange: String) -> String:
 		result += parts[i].capitalize()
 	return result
 
-func appendDir(directory_path: String, target_array: Array) -> void:
-	var dir = DirAccess.open(ProjectSettings.globalize_path("res://"))
+func appendDir(directory_path: String, target_array: Array, match_regex: String = "") -> void:
+	var dir = DirAccess.open(ProjectSettings.globalize_path(directory_path))
 	if dir == null:
-		push_error("Failed to initialize DirAccess for res://")
+		push_error("Failed to initialize DirAccess for: " + directory_path)
+		return
 
 	dir.list_dir_begin()
-	
 	var file_name = dir.get_next()
+
 	while file_name != "":
-		# Form the full path for the file.
-		var full_path = directory_path + "/" + file_name
-		# Check that the current item is not a directory.
 		if not dir.current_is_dir():
-			target_array.append(full_path)
+			var full_path = directory_path + "/" + file_name
+			if match_regex == "" or file_name.matchn(match_regex):
+				target_array.append(full_path)
 		file_name = dir.get_next()
-	
+
 	dir.list_dir_end()
