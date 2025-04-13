5051fce4ef67e5d4d0debf60b004a08da6a7a472
fauly <faulyfaux@gmail.com>
2025-04-13 23:19:06 +0700

    Basis for MovementController

---
+++ character/controllers/MovementController.gd
@@ -0,0 +1,43 @@
+@tool
+extends Node
+class_name MovementController
+
+@export var modules_dir: String = "res://Character/Modules/Movement"
+@export var velocity: Vector3 = Vector3.ZERO
+
+var modules: Array[MovementModule] = []
+
+func _ready():
+	_load_modules_from_dir()
+
+func _physics_process(delta: float):
+	for m in modules:
+		if m.enabled:
+			velocity = m.apply(velocity, delta)
+
+	# You might call `move_and_slide(velocity)` from parent script
+
+func handle_input(action: String, value: Variant):
+	for m in modules:
+		if m.enabled and m.has_method("handle_input"):
+			m.handle_input(action, value)
+
+func _load_modules_from_dir():
+	for child in get_children():
+		if child is MovementModule:
+			remove_child(child)
+			child.queue_free()
+
+	modules.clear()
+
+	# Load new
+	var paths: Array = []
+	ut.appendDir(modules_dir, paths, "*.gd")
+
+	for path in paths:
+		var script = load(path)
+		if script and script is GDScript:
+			var node: Node = script.new()
+			if node is MovementModule:
+				add_child(node)
+				modules.append(node)
---
+++ character/modules/movement/MovementModule.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name MovementModule
+
+@export var enabled: bool = true
+@export var priority: int = 0
+
+func apply(velocity: Vector3, _delta: float) -> Vector3:
+	return velocity
+
+func handle_input(_action: String, _value: Variant) -> void:
+	pass
